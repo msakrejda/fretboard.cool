d4b415919fa4d7ebfbb2d34126da4052a150864e
Maciek Sakrejda <m.sakrejda@gmail.com>
2020-02-22 22:37:07 -0800

    Fix dominant 7 chord definition

--- src/theory/chord.ts
+++ src/theory/chord.ts
@@ -6,7 +6,7 @@ export type Chord = PitchClassSequence;
 export const ChordKinds = {
   major: [ P(1), M(3), P(5) ],
   minor: [ P(1), m(3), P(5) ],
-  'dominant 7': [ P(1), m(3), P(5), m(7) ],
+  'dominant 7': [ P(1), M(3), P(5), m(7) ],
 } as const;
 
 export type ChordKind = keyof typeof ChordKinds;
