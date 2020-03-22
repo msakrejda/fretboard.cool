08739ca1269859c8890d76e06c4b92ed9abc9697
Maciek Sakrejda <m.sakrejda@gmail.com>
2020-03-21 17:18:22 -0700

    Prettify

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -56,7 +56,8 @@ export const Main: React.FC = () => {
 
     if (!soundPlayer) {
       setPendingPlayback(marker.note)
-      const ac = new (AudioContext || (window as unknown as MaybeSafariWindow).webkitAudioContext)()
+      const ac = new (AudioContext ||
+        ((window as unknown) as MaybeSafariWindow).webkitAudioContext)()
       const instrumentPath = (process.env.PUBLIC_URL +
         '/fluid-r3-acoustic_guitar_steel-mp3.js') as InstrumentName
       Soundfont.instrument(ac, instrumentPath).then((guitar) => {
